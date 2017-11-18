3ec881489fa989710350d8db8ce0e1f3d5b5a86f
kendi ardi <arsamisapper12@gmail.com>
2017-11-18 08:24:08 +0800

    Void

--- Training2.cpp
+++ Training2.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+void input(int& kaki, double& inci); 
+void convert (int kaki, double inci, double& meter);  
+void output(int kaki, double inci, double meter);
+
 int main(){
 	int kaki;
 	double inci, meter;
