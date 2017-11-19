177fd8e8707604e5b1d7fa931af85df76a9e4310
kendi ardi <arsamisapper12@gmail.com>
2017-11-20 07:33:40 +0800

    Void

--- Training1.cpp
+++ Training1.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+void input(int& hours24, int& menit);
+void convert(int& jam, char& AMPM);
+void output(int jam, int menit, char AMPM);
+
 int main(){
 	
 }
